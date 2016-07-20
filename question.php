647634b7b60679cd99361f8a232ecbfc9716403d
Gerard Cuello <gerard.urv@gmail.com>
2016-07-20 17:51:46 +0200

    Code improvements

--- question.php
+++ question.php
@@ -405,7 +405,6 @@ class qtype_programmedresp_question extends question_graded_automatically {
         if (!is_array($results)) {
             $results = array($results);
         }
-        print_object($results);
 
         return $results;
     }
